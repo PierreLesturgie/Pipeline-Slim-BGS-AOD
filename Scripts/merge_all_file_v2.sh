60c67ae07daa92ffffa5e47bb5a9b234dc8d85ce
Pierre Lesturgie <62964695+PierreLesturgie@users.noreply.github.com>
2026-05-18 11:00:41 +0100

    Create merge_all_file_v2.sh

---
+++ Scripts/merge_all_file_v2.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+cd divdel_mig
+tag="divdel"
+# Get the list of slim input files in folder 
+# i.e. files that have tag and end in .s
+FILES=`ls | grep "r2.5e-8"`
+
+# create one script per file and per run to run SLIM and launch it on the cluster
+for file in ${FILES}; 
+do
+	filename=$(basename "$file") # this one is useful in case there is a path
+    prefix="${filename%/}" # get the prefix, i.e., without ".s"
+    #reco="${prefix%_*}" # to extract directly the recombination rate
+    #reco="${reco##*_}" # to extract directly the recombination rate
+    #reco="${reco:1}" # to extract directly the recombination rate
+    
+    #mkdir $prefix
+	
+    #outputfile=Rstate_${prefix}_\${task}\${TASK_ID}.txt
+
+	cd $prefix
+		cat Rstate* >> ./../Rstate_mig_all_v4.txt
+    cd ..
+done
+echo "mig done"
+
+cd ../divdel_no_mig
+
+# Get the list of slim input files in folder 
+# i.e. files that have tag and end in .s
+FILES=`ls | grep "r2.5e-8"`
+
+# create one script per file and per run to run SLIM and launch it on the cluster
+for file in ${FILES}; 
+do
+	filename=$(basename "$file") # this one is useful in case there is a path
+    prefix="${filename%/}" # get the prefix, i.e., without ".s"
+    #reco="${prefix%_*}" # to extract directly the recombination rate
+    #reco="${reco##*_}" # to extract directly the recombination rate
+    #reco="${reco:1}" # to extract directly the recombination rate
+    
+    #mkdir $prefix
+	
+    #outputfile=Rstate_${prefix}_\${task}\${TASK_ID}.txt
+
+	cd $prefix
+		cat Rstate* >> ./../Rstate_nomig_all_v4.txt
+    cd ..
+done
